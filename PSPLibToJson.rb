c099885bdcdbfdd753af9c074256d690acaaeb80
André Schnabel <andreschnabel@me.com>
2015-04-09 14:40:41 +0200

    Beautification

--- PSPLibToJson.rb
+++ PSPLibToJson.rb
@@ -4,44 +4,43 @@
 
 require 'json'
 
-def jobDatas(n, nj); Array.new(n) { Array.new nj } end
-def indexWithStart(lines, start); lines.index { |line| line.start_with? start } end
-def ints(strs); strs.map { |s| s.to_i } end
-def jsonify(hsh); JSON.pretty_generate hsh end 
+def job_arrays(n, nj); Array.new(n) { Array.new nj } end
+def ix_line_with_prefix(lines, start); lines.index { |line| line.start_with? start } end
+def ints(strings); strings.map { |s| s.to_i } end
+def pretty_json(hsh); JSON.pretty_generate hsh end
 
 def main(args)
-	inputFilename = if args.length > 0 then args[0] else "example.sm" end
-	outputFilename = inputFilename.sub '.sm', '.json'
-	content = IO.read inputFilename
+	input_filename = (args.length > 0) ? args[0] : 'example.sm'
+	output_filename = input_filename.sub '.sm', '.json'
+	content = IO.read input_filename
 	lines = content.lines
 
-	numJobs = content.match(/^jobs[^\d]+(\d+)/).captures.first.to_i
-	numRes = content.match(/[^n]renewable[^\d]+(\d+)/).captures.first.to_i
+	num_jobs = content.match(/^jobs[^\d]+(\d+)/).captures.first.to_i
+	num_res = content.match(/[^n]renewable[^\d]+(\d+)/).captures.first.to_i
 
-	succs, jobs, durations = jobDatas 3, numJobs
-	demands = Array.new(numJobs) { Array.new numRes }
-	capacities = Array.new(numRes)
+	successors, jobs, durations = job_arrays 3, num_jobs
+	demands = Array.new(num_jobs) { Array.new num_res }
 
-	precedenceOffset = 2 + (indexWithStart lines, "PRECEDENCE RELATIONS:")
-	reqDurOffset = 3 + (indexWithStart lines, "REQUESTS/DURATIONS:")		
-	(0..numJobs-1).each { |i|
-		parts = lines[precedenceOffset+i].split
+	precedence_offset = 2 + (ix_line_with_prefix lines, 'PRECEDENCE RELATIONS:')
+	requirements_durations_offset = 3 + (ix_line_with_prefix lines, 'REQUESTS/DURATIONS:')
+	(0..num_jobs-1).each { |i|
+		parts = lines[precedence_offset+i].split
 		jobs[i] = parts.first.to_i
-		succs[i] = ints(parts.drop(3))
+		successors[i] = ints(parts.drop(3))
 
-		parts = lines[reqDurOffset+i].split
+		parts = lines[requirements_durations_offset+i].split
 		durations[i] = parts[2].to_i
-		(0..numRes-1).each { |r|
+		(0..num_res-1).each { |r|
 			demands[i][r] = parts[3+r].to_i
 		}
 	}
 
-	resCapOffset = 2 + (indexWithStart lines, "RESOURCEAVAILABILITIES:")
-	capacities = ints lines[resCapOffset].split
+	resource_capacities_offset = 2 + (ix_line_with_prefix lines, 'RESOURCEAVAILABILITIES:')
+	capacities = ints lines[resource_capacities_offset].split
 
-	projHash = {jobs: jobs, durations: durations, succs: succs, demands: demands, capacities: capacities}
+	project_hash = {jobs: jobs, durations: durations, succs: successors, demands: demands, capacities: capacities}
 
-	IO.write outputFilename, (jsonify projHash)
+	IO.write output_filename, (pretty_json project_hash)
 end
 
 main(ARGV)
